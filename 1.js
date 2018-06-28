ab2ac361a1426a3e583369391f74ff937dd16b9b
kiminjong <phamloc611@gmail.com>
2018-06-29 02:39:27 +0700

    creative

---
+++ 1.js
@@ -0,0 +1,13 @@
+﻿ $(function(){
+ 	/*mouse scroll*/
+	 $(window).scroll(function() {
+	 	var currentposition = $('html, body').scrollTop();
+	 	if(currentposition>100){
+	 		$('.mainmenu').addClass('newmenu');
+	 	}
+	 	else{
+	 		$('.mainmenu').removeClass('newmenu');
+	 	}
+	});
+})  
+ 
